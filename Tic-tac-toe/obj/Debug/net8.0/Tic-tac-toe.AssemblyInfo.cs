956b52b92df1abbc1c064a5733d50b6204c610ca
Pah1ava <emma.votson00@gmail.com>
2025-01-30 19:38:16 +0200

    Initial commit

--- Tic-tac-toe/obj/Debug/net8.0/Tic-tac-toe.AssemblyInfo.cs
+++ Tic-tac-toe/obj/Debug/net8.0/Tic-tac-toe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Tic-tac-toe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7520a4adbbff34f0c37cb1229c47909c62b21520")]
 [assembly: System.Reflection.AssemblyProductAttribute("Tic-tac-toe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Tic-tac-toe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
