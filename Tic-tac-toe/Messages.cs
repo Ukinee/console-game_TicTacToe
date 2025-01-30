5aa84aa3f73a9d76878dcfa3ae80623e49006618
Pah1ava <emma.votson00@gmail.com>
2025-01-30 19:39:41 +0200

    add Massages

---
+++ Tic-tac-toe/Messages.cs
@@ -0,0 +1,32 @@
+﻿namespace TicTacToe
+{
+    internal class Messages
+    {
+        public static void DisplayWelcomeMessage()
+        {
+
+        }
+
+        public static void DisplayWinMessage(char currentPlayer)
+        {
+            Console.Clear();
+            string winMessage = File.ReadAllText("Win.txt");
+
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine(winMessage);
+            Console.ResetColor();
+            Console.ReadKey();
+
+            Program.ShowMenu();
+        }
+
+        public static void DisplayDrawMessage()
+        {
+            Console.Clear();
+            string drawMessage = File.ReadAllText("Draw.txt");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine(drawMessage);
+            Console.ResetColor();
+        }
+    }
+}
